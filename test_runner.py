95900bd6bb49d21bc8c6c61fc08bbe66470849e2
whateverx88 <whateverx88@gmail.com>
2026-05-18 23:21:54 +0200

    kosmetyczne zmiany do test runnera

--- test_runner.py
+++ test_runner.py
@@ -5,7 +5,7 @@ loader = unittest.TestLoader()
 suite = loader.discover('tests')
 
 runner = HtmlTestRunner.HTMLTestRunner(
-    output='reports',
+    output='test_reports',
     report_name='Test Report',
     report_title='Python/Selenium Automation Report',
     combine_reports = True
